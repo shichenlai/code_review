8569509d101da757c5813e0cf7afb0fd1efca4f6
shichenlai <shichenlai@gmail.com>
2022-04-07 13:50:36 -0500

    add doc

---
+++ doc_practice/mathops/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is the main module for mathops.
+"""
+__all__ = ["summation", "__version__"]
+
+from .version import __version__
---
+++ doc_practice/mathops/summation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def summation(a):
+  """
+  Return summation result as floats.
+
+  :param a: array a
+  :type a: array
+  :return: the sum
+  :rtype: float
+  """
+  return np.sum(a)
